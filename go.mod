15d22ef999495c720727ccb59404873474b48f89
Devin Ward <devinw1325wc@gmail.com>
2023-03-15 16:32:42 -0600

    moved data structures to new repo

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module github.com/devsquared/gobber
 
 go 1.20
 
-require github.com/google/go-cmp v0.5.9 // indirect
--- pkg/queue/queue.go
+++
@@ -1,7 +0,0 @@
-package queue
-
-type Queue interface {
-	Peek() (any, error)
-	Pop() (any, error)
-	Push(element any)
-}
